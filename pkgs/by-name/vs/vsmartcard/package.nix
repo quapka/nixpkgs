bf3112780791e821c225f0b99192dded76ef546f
quapka <quapka@gmail.com>
2025-05-02 10:42:42 +0200

    Add Virtual Smart Card plugin for PC/SC

---
+++ pkgs/by-name/vs/vsmartcard/package.nix
@@ -0,0 +1,53 @@
+{
+  stdenv,
+  fetchFromGitHub,
+  automake,
+  autoconf,
+  libtool,
+  pkg-config,
+  help2man,
+  pcsclite,
+  python3,
+}:
+stdenv.mkDerivation rec {
+  pname = "vsmartcard";
+  name = pname;
+  vesion = "0.10";
+
+  src = fetchFromGitHub {
+    owner = "frankmorgner";
+    repo = "vsmartcard";
+    rev = "virtualsmartcard-0.10";
+    hash = "sha256-+BrX2aqByUvIUbN4K+sdq9bH29FD2rtTt4q+URPgx7A=";
+  };
+
+  srcDirectory = "./virtualsmartcard/";
+
+  configurePhase = ''
+    pushd ${srcDirectory}
+    autoreconf -vis
+    ./configure --prefix $out
+    popd
+  '';
+
+  installPhase = ''
+    pushd ${srcDirectory}
+    make install
+    popd
+  '';
+
+  # local Python package needs to be available
+  fixupPhase = ''
+    cp --recursive ${srcDirectory}/src/vpicc/virtualsmartcard $out/bin
+  '';
+
+  nativeBuildInputs = [
+    automake
+    autoconf
+    libtool
+    help2man
+    pkg-config
+    pcsclite
+    python3
+  ];
+}
